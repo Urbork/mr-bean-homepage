c9be7c64393bdf16a7f32197667de6b1f1ded1c5
Jonasz Ja <jajonaszjj@gmail.com>
2022-12-05 10:57:14 +0100

    just cosmetic changes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,14 @@
-let buttonElement = document.querySelector(".js-button");
-let characterImage = document.querySelector(".js-image");
+{
+  const buttonElement = document.querySelector(".js-button");
+  const characterImage = document.querySelector(".js-image");
 
-// buttonElement.addEventListener("click", () => {
-//   switch (characterImage.getAttribute("src")) {
-//     case "https://i.postimg.cc/65TRDMkQ/0001.webp":
-//       characterImage.setAttribute(
-//         "src",
-//         "https://i.postimg.cc/LsF1g2nq/cartoon.png"
-//       );
-//       buttonElement.innerText = "Zmień Jasia w człowieka";
-//       break;
-//     default:
-//       characterImage.setAttribute(
-//         "src",
-//         "https://i.postimg.cc/65TRDMkQ/0001.webp"
-//       );
-//       buttonElement.innerText = "Zmień w kreskówkową postać";
-//   }
-// });
-
-buttonElement.addEventListener("click", () => {
-  if (
-    characterImage.getAttribute("src") ===
-    "https://i.postimg.cc/65TRDMkQ/0001.webp"
-  ) {
-    characterImage.setAttribute(
-      "src",
-      "https://i.postimg.cc/LsF1g2nq/cartoon.png"
-    );
-    buttonElement.innerText = "Zmień Jasia w człowieka";
-  } else {
-    characterImage.setAttribute(
-      "src",
-      "https://i.postimg.cc/65TRDMkQ/0001.webp"
-    );
-    buttonElement.innerText = "Zmień w kreskówkową postać";
-  }
-});
+  buttonElement.addEventListener("click", () => {
+    if (characterImage.getAttribute("src") === "./images/0001.webp") {
+      characterImage.setAttribute("src", "./images/cartoon.png");
+      buttonElement.innerText = "Zmień Jasia w człowieka";
+    } else {
+      characterImage.setAttribute("src", "./images/0001.webp");
+      buttonElement.innerText = "Zmień w kreskówkową postać";
+    }
+  });
+}
